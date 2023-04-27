8a1fe0d860f510cf99293140b5bd519abc5b7275
OkejoTM <denastytiko03@mail.ru>
2023-04-27 19:34:21 +0300

    add function to read file data

--- CalculateCheapestPath/CalculateCheapestPath.cpp
+++ CalculateCheapestPath/CalculateCheapestPath.cpp
@@ -1,9 +1,81 @@
 ﻿#include <iostream>
-
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <queue>
 using namespace std;
 
 
+vector<string> readDataFromFile(vector<vector<int>>& roads, vector<int>& costs)
+{
+	// Открыть файл
+	ifstream file;
+	file.open("input.csv");
+
+	// Считать первую строку файла
+	string line;
+	getline(file, line);
+	stringstream ss(line);
+
+	string label;
+	vector<string> citiesLabes;
+
+
+	// Заполнить вектор названиями городов
+	while (getline(ss, label, ';')) {
+		if (label != "") {
+			citiesLabes.push_back(label);
+		}
+	}
+	roads.resize(citiesLabes.size(), vector<int>(citiesLabes.size(), 0));
+	costs.resize(citiesLabes.size(), 0);
+
+	getline(file, line);
+	stringstream ssCost(line);
+	int costValueCol = 0;
+	string costVal;
+
+	// Считывание стоимости дорог
+	while (getline(ssCost, costVal, ';')) {
+		if (costVal != "") {
+			costs[costValueCol] = stoi(costVal);
+		}
+		costValueCol++;
+	}
+
+	getline(file, line); // Считать пустую строку
+	getline(file, line); // Считать названия город
+
+	//заполнение матрицы смежности из файла
+	int row = 0;
+	while (getline(file, line)) {
+		stringstream ss(line);
+		string value;
+		int col = 0;
+		while (getline(ss, value, ';')) {
+			if (value != "" && col > 0) {
+				roads[row][col - 1] = stoi(value);
+			}
+			col++;
+		}
+		row++;
+	}
+
+	file.close();
+
+	// Вернуть вектор названий городов
+	return citiesLabes;
+}
+
+
 int main() {
+
+	//Считать данные из файла
+
+	// Преобразовать дороги и стоимости в матрицу смежности
+
+	// Вывести решение в файл
 
 	return 0;
 }
